2e07311fbeb6a2e1614a938a4945796425526489
Walid Omar <walidomar291@gmail.com>
2024-03-15 11:29:55 +0200

    single responsibility principle code

---
+++ SOLID_SingleResponsibility/Program.cs
@@ -0,0 +1,72 @@
+﻿using System.Diagnostics;
+
+namespace App
+{
+    class Program
+    {
+        static void Main()
+        {
+            var j = new Journal();
+            j.AddEntry("I cried today.");
+            j.AddEntry("I ate a bug.");
+            Console.WriteLine(j);
+
+            var p = new Persistence();
+
+            //Edit the file path first before you run the code
+            var filename = @"insert the file path you want";
+            p.SaveToFile(j, filename);
+            Process.Start(filename);
+
+        }
+    }
+
+    public class Journal
+    {
+        private readonly List<string> entries = new List<string>();
+
+        private static int count = 0;
+
+        public int AddEntry(string text)
+        {
+            entries.Add($"{++count}: {text}");
+            return count; // memento pattern!
+        }
+
+        public void RemoveEntry(int index)
+        {
+            entries.RemoveAt(index);
+        }
+
+        public override string ToString()
+        {
+            return string.Join(Environment.NewLine, entries);
+        }
+
+        // breaks single responsibility principle 
+        public void Save(string filename, bool overwrite = false)
+        {
+            File.WriteAllText(filename, ToString());
+        }
+
+        public void Load(string filename)
+        {
+
+        }
+
+        public void Load(Uri uri)
+        {
+
+        }
+    }
+
+    // handles the responsibility of persisting objects
+    public class Persistence
+    {
+        public void SaveToFile(Journal journal, string filename, bool overwrite = false)
+        {
+            if (overwrite || !File.Exists(filename))
+                File.WriteAllText(filename, journal.ToString());
+        }
+    }
+}
